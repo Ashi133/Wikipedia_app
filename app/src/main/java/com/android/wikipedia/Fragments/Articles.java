37b1d7e1db814749ff1e93b8569ccf7c77d478d3
Riyaaz <rahmatullaha845@gmail.com>
2022-05-06 11:51:04 +0530

    Wikipedia-0.0.3

--- app/src/main/java/com/android/wikipedia/Fragments/Articles.java
+++ app/src/main/java/com/android/wikipedia/Fragments/Articles.java
@@ -1,4 +1,5 @@
 package com.android.wikipedia.Fragments;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -6,12 +7,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.wikipedia.Adapter.ArticlesAdapter;
 import com.android.wikipedia.R;
+import com.android.wikipedia.UpdateTheme;
+import com.android.wikipedia.contentLoader;
 import com.cooltechworks.views.shimmer.ShimmerRecyclerView;
 import com.google.firebase.database.DatabaseReference;
 
@@ -22,6 +26,7 @@ public class Articles extends Fragment {
     SwipeRefreshLayout refreshLayout;
     DatabaseReference reference;
     //FirebaseAuth auth;
+    @SuppressLint ("LongLogTag")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -32,6 +37,8 @@ public class Articles extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
         recyclerView.hideShimmerAdapter();
+        String url="https://commons.wikimedia.org/w/api.php?action=query&prop=imageinfo&iiprop=timestamp|user|url&generator=categorymembers&gcmtype=file&gcmtitle=Category:Featured_pictures_on_Wikimedia_Commons&format=json&utf8";
+        contentLoader.loadArticle(requireActivity(),url);
         return view;
     }
     public  void search(String text){
---
+++ app/src/main/java/com/android/wikipedia/contentLoader.java
@@ -0,0 +1,47 @@
+package com.android.wikipedia;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class contentLoader {
+    @SuppressLint ("StaticFieldLeak")
+    public static Context mContext;
+    public static void loadArticle(Context context, String url){
+        mContext=context;
+        loader(url);
+    }
+    public static void loadImage(Context context,String url){
+        mContext=context;
+    }
+    public static void loadCategory(Context context,String url){
+        mContext=context;
+    }
+    public static void loader(String url){
+        RequestQueue requestQueue;
+        requestQueue= Volley.newRequestQueue(mContext);
+        JsonObjectRequest jsonArrayRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Result ====", String.valueOf(response));
+            }
+        }, new Response.ErrorListener() {
+            @SuppressLint ("LongLogTag")
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(mContext, "Response == "+error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+}
